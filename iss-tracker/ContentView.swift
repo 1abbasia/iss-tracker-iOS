35faae606bb2a9aa2d3e19eaf3af7d9dd60af13b
Ahsan Abbasi <1abbasia@gmail.com>
2023-03-16 16:20:54 -0400

    started swift dev environment

---
+++ iss-tracker/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  iss-tracker
+//
+//  Created by Ahsan on 3/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("yoooo")
+            HStack {
+                Text("hello")
+                Text("yerr")
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iss-tracker/iss_trackerApp.swift
@@ -0,0 +1,23 @@
+//
+//  iss_trackerApp.swift
+//  iss-tracker
+//
+//  Created by Ahsan on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct iss_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct Previews_iss_trackerApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
